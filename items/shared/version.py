7fc454bbe6160891321a2f3fa972c871618ef862
SwatKat1977 <paulmorriss1977@gmail.com>
2025-01-22 19:43:09 +0000

    more fixes for docstrings

--- items/shared/version.py
+++ items/shared/version.py
@@ -1,4 +1,4 @@
-'''
+"""
 Copyright 2025 Integrated Test Management Suite Development Team
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +12,7 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-'''
+"""
 
 RELEASE_VERSION = '0.0.0'
 
