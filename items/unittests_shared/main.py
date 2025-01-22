f885b84729c2966b4fd0cb481561c38cc356d10d
SwatKat1977 <paulmorriss1977@gmail.com>
2025-01-22 20:27:12 +0000

    added thread safe singleton, plus unit tests for it

--- items/unittests_shared/main.py
+++ items/unittests_shared/main.py
@@ -2,6 +2,7 @@ import unittest
 from test_base_application import TestBaseApplication
 from test_base_view import TestBaseView
 from test_configuration_manager import TestConfigurationManager
+from test_thread_safe_singleton import TestThreadSafeSingleton
 
 if __name__ == "__main__":
     unittest.main()
