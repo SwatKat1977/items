39d0594c9072aa7fbee41df12e1dd9c12183c760
SwatKat1977 <paulmorriss1977@gmail.com>
2025-01-23 20:03:01 +0000

    added header

--- items/shared/base_sqlite_interface.py
+++ items/shared/base_sqlite_interface.py
@@ -1,5 +1,5 @@
 """
-Copyright 2014-2025 Integrated Test Management Suite
+Copyright 2025 Integrated Test Management Suite Development Team
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
