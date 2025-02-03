8bee1045b98110d13be35cfe0cd83a98c669225e
SwatKat1977 <paulmorriss1977@gmail.com>
2025-02-03 19:38:42 +0000

    docstring

--- items/accounts_svc/state_object.py
+++ items/accounts_svc/state_object.py
@@ -19,6 +19,23 @@ from service_health_enums import ComponentDegradationLevel
 
 @dataclass
 class StateObject:
+    """
+    Represents the state of a service, including its health status, database
+    status, version, and startup time.
+
+    Attributes:
+        service_health (ComponentDegradationLevel): The current health status
+                                                    of the service.
+        service_health_state_str (str): A descriptive string representing the
+                                        service health state.
+        database_health (ComponentDegradationLevel): The current health status
+                                                     of the database.
+        database_health_state_str (str): A descriptive string representing the
+                                         database health state.
+        version (str): The version of the service.
+        startup_time (int): The timestamp (Unix time) when the service was
+                            started.
+    """
     service_health: ComponentDegradationLevel = ComponentDegradationLevel.NONE
     service_health_state_str: str = ""
     database_health: ComponentDegradationLevel = ComponentDegradationLevel.NONE
