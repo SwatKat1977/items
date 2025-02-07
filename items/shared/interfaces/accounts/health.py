c60ed51f1cc94ef83abea5c05f24557a01bd8caf
SwatKat1977 <paulmorriss1977@gmail.com>
2025-02-07 20:17:24 +0000

    fix pylint warning

--- items/shared/interfaces/accounts/health.py
+++ items/shared/interfaces/accounts/health.py
@@ -13,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+# pylint: disable=R0801
 
 SCHEMA_HEALTH_RESPONSE: dict = {
     "$schema": "http://json-schema.org/draft-07/schema#",
