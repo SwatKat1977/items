f20b500cef43f127e9ca3260bc056aac76c2484f
SwatKat1977 <paulmorriss1977@gmail.com>
2025-02-16 04:59:58 +0000

    unit tests for health api

--- items/unittests_cms_svc/main.py
+++ items/unittests_cms_svc/main.py
@@ -2,6 +2,7 @@ import unittest
 from test_application import TestApplication
 from test_threadsafe_configuration import TestThreadSafeConfiguration
 from test_sqlite_interface import TestSqliteInterface
+from test_apis_health_api import TestApiHealthApiView
 
 if __name__ == "__main__":
     unittest.main()
