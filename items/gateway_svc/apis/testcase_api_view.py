490b724897b48603208149b6f28a1e12c93ed37d
SwatKat1977 <paulmorriss1977@gmail.com>
2025-02-21 00:43:55 +0000

    implemented /<int:project_id>/testcase/testcases_details

--- items/gateway_svc/apis/testcase_api_view.py
+++ items/gateway_svc/apis/testcase_api_view.py
@@ -70,7 +70,43 @@ class TestCaseApiView(BaseView):
                               status=http.HTTPStatus.OK,
                               content_type="application/json")
 
-    async def get_testcase(self, request_msg: ApiResponse,
+    async def get_testcase(self,
                            project_id: int,
                            case_id: int) -> quart.Response:
-        ...
+
+        cms_svc: str = ThreadSafeConfiguration().apis_cms_svc
+
+        request_body: dict = {
+            "project_id": project_id
+        }
+        details_url: str = f"{cms_svc}testcases/get_case/{case_id}"
+
+        try:
+            api_response = await self._call_api_post(details_url, request_body)
+
+            if api_response.status_code != http.HTTPStatus.OK:
+                self._logger.critical("CMS svc /testcases/get_case request invalid"
+                                      " - Reason: %s",api_response.exception_msg)
+                response_json = {
+                    "status": 0,
+                    'error': 'Internal error!'
+                }
+                return quart.Response(json.dumps(response_json),
+                                      status=http.HTTPStatus.INTERNAL_SERVER_ERROR,
+                                      content_type="application/json")
+
+        except requests.exceptions.ConnectionError as ex:
+            except_str = f"Internal error: {ex}"
+            self._logger.error(except_str)
+
+            response_json = {
+                "status":  0,
+                "error": str(ex)
+            }
+            return quart.Response(json.dumps(response_json),
+                                  status=http.HTTPStatus.INTERNAL_SERVER_ERROR,
+                                  content_type="application/json")
+
+        return quart.Response(json.dumps(api_response.body),
+                              status=http.HTTPStatus.OK,
+                              content_type="application/json")
