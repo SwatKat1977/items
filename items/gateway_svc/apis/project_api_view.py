171f6b85e5de41c7e5eff902994bd7e418f90d6f
SwatKat1977 <paulmorriss1977@gmail.com>
2025-03-02 21:22:54 +0000

    work on gateway svc - project API

--- items/gateway_svc/apis/project_api_view.py
+++ items/gateway_svc/apis/project_api_view.py
@@ -33,51 +33,28 @@ class ProjectApiView(BaseView):
         # Get fields from query parameters
         value_fields = quart.request.args.get("value_fields")
         count_fields = quart.request.args.get("count_fields")
-
         value_fields_str = f"value_fields={value_fields}" if value_fields \
             else ""
         count_fields_str = f"count_fields={count_fields}" if count_fields \
             else ""
         joiner_str = "&" if value_fields and count_fields else ""
-
-        # http://127.0.0.1:4000/project/overviews?count_fields=no_of_test_runs
-
         cms_svc: str = ThreadSafeConfiguration().apis_cms_svc
+        url: str = f"{cms_svc}project/overviews?{value_fields_str}" + \
+                   f"{joiner_str}{count_fields_str}"
 
-        details_url: str = f"{cms_svc}project/overviews?{value_fields_str}" + \
-                           f"{joiner_str}{count_fields_str}"
+        api_response = await self._call_api_get(url)
 
-        print(f"Details: {details_url}")
-
-        return quart.Response(json.dumps({}),
-                              status=http.HTTPStatus.INTERNAL_SERVER_ERROR,
-                              content_type="application/json")
-
-        try:
-            api_response = await self._call_api_post(details_url, request_body)
-
-            if api_response.status_code != http.HTTPStatus.OK:
-                self._logger.critical("CMS svc /testcases/details request invalid"
-                                      " - Reason: %s",api_response.exception_msg)
-                response_json = {
-                    "status": 0,
-                    'error': 'Internal error!'
-                }
-                return quart.Response(json.dumps(response_json),
-                                      status=http.HTTPStatus.INTERNAL_SERVER_ERROR,
-                                      content_type="application/json")
-
-        except requests.exceptions.ConnectionError as ex:
-            except_str = f"Internal error: {ex}"
-            self._logger.error(except_str)
-
+        if api_response.status_code != http.HTTPStatus.OK:
+            self._logger.critical("CMS svc /project/overviews request invalid"
+                                  " - Reason: %s",api_response.exception_msg)
             response_json = {
-                "status":  0,
-                "error": str(ex)
+                "status": 0,
+                'error': 'Internal error!'
             }
             return quart.Response(json.dumps(response_json),
                                   status=http.HTTPStatus.INTERNAL_SERVER_ERROR,
                                   content_type="application/json")
 
-
-        return await view.project_overviews()
+        return quart.Response(json.dumps(api_response.body),
+                              status=http.HTTPStatus.OK,
+                              content_type="application/json")
