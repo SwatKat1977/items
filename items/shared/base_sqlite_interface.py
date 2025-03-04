2ccfed1bc5bbcb3ff8f606b1d60245a6572b5f8a
SwatKat1977 <paulmorriss1977@gmail.com>
2025-03-04 23:34:02 +0000

    added standard query to base sqlite interface

--- items/shared/base_sqlite_interface.py
+++ items/shared/base_sqlite_interface.py
@@ -180,4 +180,27 @@ class BaseSqliteInterface:
 
         except sqlite3.Error as ex:
             raise SqliteInterfaceException(
-                f"Error querying user ID: {str(ex)}") from ex
+                f"Error performing query: {str(ex)}") from ex
+
+    def query(self, query: str, params: tuple = (), commit: bool = False) -> None:
+        """
+        Execute a query without fetching results from the SQLite database.
+
+        Args:
+            query (str): The SQL query to execute.
+            params (tuple, optional): The parameters to bind to the query.
+                                      Defaults to ().
+            commit (bool): Perform commit flag.
+
+        Raises:
+            SqliteInterfaceException: If the query execution fails.
+        """
+        cursor = self._connection.cursor()
+        try:
+            cursor.execute(query, params)
+            if commit:
+                self._connection.commit()
+
+        except sqlite3.Error as ex:
+            raise SqliteInterfaceException(
+                f"Error performing query: {str(ex)}") from ex
