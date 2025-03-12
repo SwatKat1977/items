ccfd9bbbecc6dd17385a9a4493ac3ad9cfe93be5
SwatKat1977 <paulmorriss1977@gmail.com>
2025-03-12 02:27:22 +0000

    implement updatee function

--- items/web_portal_svc/apis/webhook_api_view.py
+++ items/web_portal_svc/apis/webhook_api_view.py
@@ -13,16 +13,29 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+import http
 import logging
+import quart
 from base_view import ApiResponse, BaseView, validate_json
 import interfaces.gateway.metadata  as json_schemas
+from metadata_settings import MetadataSettings
 
 
 class WebhookApiView(BaseView):
 
-    def __init__(self, logger: logging.Logger):
+    def __init__(self, logger: logging.Logger,
+                 metadata: MetadataSettings):
         self._logger = logger.getChild(__name__)
+        self._metadata: MetadataSettings = metadata
 
     @validate_json(json_schemas.SCHEMA_UPDATE_METADATA_REQUEST)
     async def update_metadata(self, request_msg: ApiResponse):
-        ...
+        body = request_msg.body
+
+        self._metadata.default_time_zone = body.default_time_zone
+        self._metadata.using_server_default_time_zone = body.using_server_default_time_zone
+        self._metadata.instance_name = body.instance_name
+
+        return quart.Response("OK",
+                              status=http.HTTPStatus.OK,
+                              content_type="text/plain")
