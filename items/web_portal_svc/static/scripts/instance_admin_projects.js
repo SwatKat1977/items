015dd2da0b9235e03956e4322fedeea67d3db3c8
SwatKat1977 <paulmorriss1977@gmail.com>
2025-03-24 18:27:23 +0000

    project html

--- items/web_portal_svc/static/scripts/instance_admin_projects.js
+++ items/web_portal_svc/static/scripts/instance_admin_projects.js
@@ -1,34 +1,20 @@
-  document.getElementById('confirmCheckbox').addEventListener('change', function() {
-    const confirmButton = document.getElementById('confirmDeleteButton');
-    confirmButton.disabled = !this.checked;
-  });
+document.getElementById('confirmCheckbox').addEventListener('change', function() {
+  document.getElementById('confirmDeleteButton').disabled = !this.checked;
+});
 
-  document.getElementById('confirmDeleteButton').addEventListener('click', function() {
-    const projectId = document.querySelector('[data-project-id]').getAttribute('data-project-id');
+document.getElementById('confirmDeleteButton').addEventListener('click', function() {
+  const projectId = document.getElementById('confirmDeleteModal').getAttribute('data-project-id');
+  document.getElementById('projectId').value = projectId;
+  document.getElementById('deleteForm').submit();
+});
 
-    // Set the projectId in the hidden form
-    document.getElementById('projectId').value = projectId;
-
-    // Submit the form
-    document.getElementById('deleteForm').submit();
-  });
-
-  // Reset checkbox and button when modal is hidden
-  const modalElement = document.getElementById('confirmDeleteModal');
-  const modal = new bootstrap.Modal(modalElement);
-  modalElement.addEventListener('hidden.bs.modal', function () {
-    document.getElementById('confirmCheckbox').checked = false;
-    document.getElementById('confirmDeleteButton').disabled = true;
-  });
+document.getElementById('confirmDeleteModal').addEventListener('show.bs.modal', function (event) {
+  const button = event.relatedTarget;
+  const projectId = button.getAttribute('data-project-id');
+  this.setAttribute('data-project-id', projectId);
+});
 
 document.getElementById('confirmDeleteModal').addEventListener('hidden.bs.modal', function () {
-  const checkbox = document.getElementById('confirmCheckbox');
-  const button = document.getElementById('confirmDeleteButton');
-
-  if (checkbox) {
-    checkbox.checked = false;
-  }
-  if (button) {
-    button.disabled = true;
-  }
+  document.getElementById('confirmCheckbox').checked = false;
+  document.getElementById('confirmDeleteButton').disabled = true;
 });
