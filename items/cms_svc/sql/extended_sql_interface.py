9bf755e3fab364d003d25b1b03c2b8a81b7cb76f
SwatKat1977 <paulmorriss1977@gmail.com>
2025-06-02 13:59:54 +0100

    update to base sqlite interface

--- items/cms_svc/sql/extended_sql_interface.py
+++ items/cms_svc/sql/extended_sql_interface.py
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 import logging
+import sqlite3
 import typing
 from threadsafe_configuration import ThreadSafeConfiguration as Configuration
 from base_sqlite_interface import BaseSqliteInterface, SqliteInterfaceException
