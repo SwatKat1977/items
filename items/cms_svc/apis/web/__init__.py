424bafe7c958477cb72fa7f592f60db2d4341c86
SwatKat1977 <paulmorriss1977@gmail.com>
2025-06-20 15:12:57 +0100

    work on cms

--- items/cms_svc/apis/web/__init__.py
+++ items/cms_svc/apis/web/__init__.py
@@ -13,3 +13,24 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+import logging
+import quart
+from state_object import StateObject
+
+
+def create_web_routes(logger: logging.Logger,
+                      state: StateObject) -> quart.Blueprint:
+    from .health_api import create_blueprint as create_heath_bp
+    from .projects_api import create_blueprint as create_projects_bp
+    from .testcases_api import create_blueprint as create_testcases_bp
+    from .admin import create_admin_routes
+
+    web_bp = quart.Blueprint("web_routes", __name__)
+
+    web_bp.register_blueprint(create_heath_bp(logger, state), url_prefix="/health")
+    web_bp.register_blueprint(create_projects_bp(logger, state), url_prefix="/projects")
+    web_bp.register_blueprint(create_testcases_bp(logger, state), url_prefix="/testcases")
+
+    web_bp.register_blueprint(create_admin_routes(logger, state), url_prefix="/admin")
+
+    return web_bp
