19990b4c38549a907ef5ac4cfdd0d8783065429d
SwatKat1977 <paulmorriss1977@gmail.com>
2025-06-22 02:00:08 +0100

    cleanup

--- items/unittests_cms_svc/main.py
+++ items/unittests_cms_svc/main.py
@@ -1,11 +1,12 @@
 import unittest
 from test_application import TestApplication
 from test_threadsafe_configuration import TestThreadSafeConfiguration
-from test_sqlite_interface import TestSqliteInterface
 from test_apis_health_api import TestApiHealthApiView
 from test_apis_project_api_view import TestApiProjectApiView
 from test_apis_testcases_api_view import TestApiTestCasesApiView
-
+from test_create_web_routes import TestCreateWebRoutes
+from test_sql_sql_projects import TestSqlSqlProjects
+from test_sql_sql_tc_custom_fields import TestSqlSqlTCCustomFields
 
 if __name__ == "__main__":
     unittest.main()
