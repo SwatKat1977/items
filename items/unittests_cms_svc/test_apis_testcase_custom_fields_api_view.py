3daa5018d2b0bbdf65dc502ce985a3188d7bf49e
SwatKat1977 <paulmorriss1977@gmail.com>
2025-06-24 18:12:15 +0100

    wip

--- items/unittests_cms_svc/test_apis_testcase_custom_fields_api_view.py
+++ items/unittests_cms_svc/test_apis_testcase_custom_fields_api_view.py
@@ -31,7 +31,6 @@ class TestApisTestcaseCustomFieldsApiView(unittest.IsolatedAsyncioTestCase):
     async def test_add_custom_field_custom_field_name_exists_failure(self, mock_sql_interface):
         """Test default behavior when no fields are specified"""
         mock_db = MagicMock()
-        # mock_db.projects.project_name_exists.return_value = True
         mock_db.tc_custom_fields.custom_field_name_exists.return_value = None
         mock_sql_interface.return_value = mock_db
 
@@ -67,10 +66,9 @@ class TestApisTestcaseCustomFieldsApiView(unittest.IsolatedAsyncioTestCase):
                              {"status": 0, "error": "Internal error"})
 
     @patch('apis.web.admin.testcase_custom_fields_api_view.SqlInterface')
-    async def test_add_custom_field_custom_field_name_exists(self, mock_sql_interface):
+    async def test_add_custom_field_field_name_exists(self, mock_sql_interface):
         """Test default behavior when no fields are specified"""
         mock_db = MagicMock()
-        # mock_db.projects.project_name_exists.return_value = True
         mock_db.tc_custom_fields.custom_field_name_exists.return_value = True
         mock_sql_interface.return_value = mock_db
 
@@ -99,12 +97,137 @@ class TestApisTestcaseCustomFieldsApiView(unittest.IsolatedAsyncioTestCase):
         async with self.client as client:
             response = await client.post('/web/admin/testcase_custom_fields/testcase_custom_fields',
                                          json=test_json_body)
-            print(f"\n\nResp: {await response.get_json()}")
             data = await response.get_json()
             # Assert response status
             self.assertEqual(response.status_code, http.HTTPStatus.OK)
             self.assertEqual(data,
                              {'error': 'Duplicate field_name', 'status': 0})
+
+    @patch('apis.web.admin.testcase_custom_fields_api_view.SqlInterface')
+    async def test_add_custom_field_system_name_exists(self, mock_sql_interface):
+        """Test default behavior when no fields are specified"""
+        mock_db = MagicMock()
+        mock_db.tc_custom_fields.custom_field_name_exists.return_value = False
+        mock_db.tc_custom_fields.system_name_exists.return_value = True
+        mock_sql_interface.return_value = mock_db
+
+        view = TestcaseCustomFieldsApiView(self.mock_logger, self.mock_state_object)
+
+        # Register route for testing
+        self.app.add_url_rule('/web/admin/testcase_custom_fields/testcase_custom_fields',
+                              view_func=view.add_custom_field,
+                              methods=['POST'])
+
+        test_json_body: dict = {
+            "field_name": "new type string",
+            "description": "default",
+            "system_name": "new_system_name",
+            "field_type": "String",
+            "enabled": True,
+            "is_required": False,
+            "default_value": "",
+            "applies_to_all_projects": False,
+            "projects": [
+                  "Project Delta_1",
+                  "Project Beta_2"
+            ]
+        }
+
+        async with self.client as client:
+            response = await client.post('/web/admin/testcase_custom_fields/testcase_custom_fields',
+                                         json=test_json_body)
+            data = await response.get_json()
+            # Assert response status
+            self.assertEqual(response.status_code, http.HTTPStatus.OK)
+            self.assertEqual(data,
+                             {'error': 'Duplicate system_name', 'status': 0})
+
+    @patch('apis.web.admin.testcase_custom_fields_api_view.SqlInterface')
+    async def test_add_custom_field_system_exists_failure(self, mock_sql_interface):
+        """Test default behavior when no fields are specified"""
+        mock_db = MagicMock()
+        mock_db.tc_custom_fields.custom_field_name_exists.return_value = False
+        mock_db.tc_custom_fields.system_name_exists.return_value = None
+        mock_sql_interface.return_value = mock_db
+
+        view = TestcaseCustomFieldsApiView(self.mock_logger, self.mock_state_object)
+
+        # Register route for testing
+        self.app.add_url_rule('/web/admin/testcase_custom_fields/testcase_custom_fields',
+                              view_func=view.add_custom_field,
+                              methods=['POST'])
+
+        test_json_body: dict = {
+            "field_name": "new type string",
+            "description": "default",
+            "system_name": "new_system_name",
+            "field_type": "String",
+            "enabled": True,
+            "is_required": False,
+            "default_value": "",
+            "applies_to_all_projects": False,
+            "projects": [
+                  "Project Delta_1",
+                  "Project Beta_2"
+            ]
+        }
+
+        async with self.client as client:
+            response = await client.post('/web/admin/testcase_custom_fields/testcase_custom_fields',
+                                         json=test_json_body)
+            data = await response.get_json()
+            # Assert response status
+            self.assertEqual(response.status_code, http.HTTPStatus.INTERNAL_SERVER_ERROR)
+            self.assertEqual(data,
+                             {"status": 0, "error": "Internal error"})
+
+    @patch('apis.web.admin.testcase_custom_fields_api_view.SqlInterface')
+    async def test_add_custom_field_duplicate_projects(self, mock_sql_interface):
+        """Test default behavior when no fields are specified"""
+        mock_db = MagicMock()
+        mock_db.tc_custom_fields.custom_field_name_exists.return_value = False
+        mock_db.tc_custom_fields.system_name_exists.return_value = False
+        mock_sql_interface.return_value = mock_db
+
+        view = TestcaseCustomFieldsApiView(self.mock_logger, self.mock_state_object)
+
+        # Register route for testing
+        self.app.add_url_rule('/web/admin/testcase_custom_fields/testcase_custom_fields',
+                              view_func=view.add_custom_field,
+                              methods=['POST'])
+
+        test_json_body: dict = {
+            "field_name": "new type string",
+            "description": "default",
+            "system_name": "new_system_name",
+            "field_type": "String",
+            "enabled": True,
+            "is_required": False,
+            "default_value": "",
+            "applies_to_all_projects": False,
+            "projects": [
+                "alpha",
+                "beta",
+                "alpha"
+            ]
+        }
+
+        async with self.client as client:
+            response = await client.post('/web/admin/testcase_custom_fields/testcase_custom_fields',
+                                         json=test_json_body)
+            print(f"\n\n[duplicate_projects] Resp: {await response.get_json()}")
+            data = await response.get_json()
+            # Assert response status
+            self.assertEqual(response.status_code, http.HTTPStatus.BAD_REQUEST)
+            self.assertEqual(data,
+                             {'error': 'Duplicate projects', 'status': 0})
+
+'''
+cms_svc/apis/web/admin/testcase_custom_fields_api_view.py      56     32    43%   60-115, 141-177
+cms_svc/apis/web/admin/testcase_custom_fields_api_view.py      56     27    52%   63-66, 78-115, 141-177
+cms_svc/apis/web/admin/testcase_custom_fields_api_view.py      58     23    60%   63-66, 89-116, 142-178
+cms_svc/apis/web/admin/testcase_custom_fields_api_view.py      58     23    60%   63-66, 89-116, 142-178
+'''
 
 '''
 
