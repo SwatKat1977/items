c06aecb4baf8df05f6f20877eef20509d6e80686
SwatKat1977 <paulmorriss1977@gmail.com>
2025-06-25 12:37:24 +0100

    update main

--- items/unittests_accounts_svc/main.py
+++ items/unittests_accounts_svc/main.py
@@ -2,7 +2,7 @@ import unittest
 from test_application import TestApplication
 from test_threadsafe_configuration import TestThreadSafeConfiguration
 from test_sqlite_interface import TestSqliteInterface
-from test_apis_basic_authentication_api import TestBasicAuthAPI
+from test_apis_authentication_api import TestAuthenticationAPI
 from test_apis_health_api import TestApiHealthApiView
 
 if __name__ == "__main__":
