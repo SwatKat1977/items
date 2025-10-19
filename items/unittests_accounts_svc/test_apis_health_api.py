fffc6e6625b403b6a16f1a8db17f0cca3eed9925
SwatKat1977 <paulmorriss1977@gmail.com>
2025-10-19 20:02:12 +0100

    fix accounts svc unit tests

--- items/unittests_accounts_svc/test_apis_health_api.py
+++ items/unittests_accounts_svc/test_apis_health_api.py
@@ -7,7 +7,8 @@ from quart import Response
 from http import HTTPStatus
 from apis.health_api_view import HealthApiView
 from state_object import StateObject
-import service_health_enums as health_enums
+from service_health_enums import (ComponentDegradationLevel,
+                                  ServiceDegradationStatus)
 
 
 class TestApiHealthApiView(unittest.IsolatedAsyncioTestCase):
@@ -18,8 +19,8 @@ class TestApiHealthApiView(unittest.IsolatedAsyncioTestCase):
         self.mock_state.version = "1.2.3"
 
         # Default to healthy state
-        self.mock_state.database_health = health_enums.ComponentDegradationLevel.NONE
-        self.mock_state.service_health = health_enums.ComponentDegradationLevel.NONE
+        self.mock_state.database_health = ComponentDegradationLevel.NONE
+        self.mock_state.service_health = ComponentDegradationLevel.NONE
         self.mock_state.database_health_state_str = "OK"
         self.mock_state.service_health_state_str = "OK"
 
@@ -31,58 +32,58 @@ class TestApiHealthApiView(unittest.IsolatedAsyncioTestCase):
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
         data = json.loads(await response.get_data(as_text=True))
-        self.assertEqual(data["status"], health_enums.STATUS_HEALTHY)
+        self.assertEqual(data["status"], ServiceDegradationStatus.HEALTHY.value)
         self.assertEqual(data["issues"], None)
         self.assertEqual(data["uptime_seconds"], 5000)
         self.assertEqual(data["version"], "1.2.3")
         self.assertEqual(data["dependencies"], {
-            "database": health_enums.ComponentDegradationLevelStr[health_enums.ComponentDegradationLevel.NONE],
-            "service": health_enums.ComponentDegradationLevelStr[health_enums.ComponentDegradationLevel.NONE]
+            "database": ComponentDegradationLevel.NONE.value,
+            "service": ComponentDegradationLevel.NONE.value
         })
 
     async def test_health_degraded(self):
-        self.mock_state.database_health = health_enums.ComponentDegradationLevel.PART_DEGRADED
+        self.mock_state.database_health = ComponentDegradationLevel.PART_DEGRADED
         self.mock_state.database_health_state_str = "Slow queries detected"
-        self.mock_state.service_health = health_enums.ComponentDegradationLevel.NONE
+        self.mock_state.service_health = ComponentDegradationLevel.NONE
 
         response = await self.view.health()
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
         data = json.loads(await response.get_data(as_text=True))
-        self.assertEqual(data["status"], health_enums.STATUS_DEGRADED)
+        self.assertEqual(data["status"], ServiceDegradationStatus.DEGRADED.value)
         self.assertEqual(len(data["issues"]), 1)
         self.assertEqual(data["issues"][0]["component"], "database")
         self.assertEqual(data["issues"][0]["status"],
-                         health_enums.ComponentDegradationLevelStr[health_enums.ComponentDegradationLevel.PART_DEGRADED])
+                         ComponentDegradationLevel.PART_DEGRADED.value)
         self.assertEqual(data["issues"][0]["details"], "Slow queries detected")
 
     async def test_health_critical(self):
-        self.mock_state.database_health = health_enums.ComponentDegradationLevel.FULLY_DEGRADED
+        self.mock_state.database_health = ComponentDegradationLevel.FULLY_DEGRADED
         self.mock_state.database_health_state_str = "Database down"
-        self.mock_state.service_health = health_enums.ComponentDegradationLevel.NONE
+        self.mock_state.service_health = ComponentDegradationLevel.NONE
 
         response = await self.view.health()
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
         data = json.loads(await response.get_data(as_text=True))
-        self.assertEqual(data["status"], health_enums.STATUS_CRITICAL)
+        self.assertEqual(data["status"], ServiceDegradationStatus.CRITICAL.value)
         self.assertEqual(len(data["issues"]), 1)
         self.assertEqual(data["issues"][0]["component"], "database")
         self.assertEqual(data["issues"][0]["status"],
-                         health_enums.ComponentDegradationLevelStr[health_enums.ComponentDegradationLevel.FULLY_DEGRADED])
+                         ComponentDegradationLevel.FULLY_DEGRADED.value)
         self.assertEqual(data["issues"][0]["details"], "Database down")
 
     async def test_health_multiple_issues(self):
-        self.mock_state.database_health = health_enums.ComponentDegradationLevel.PART_DEGRADED
+        self.mock_state.database_health = ComponentDegradationLevel.PART_DEGRADED
         self.mock_state.database_health_state_str = "Slow queries detected"
-        self.mock_state.service_health = health_enums.ComponentDegradationLevel.FULLY_DEGRADED
+        self.mock_state.service_health = ComponentDegradationLevel.FULLY_DEGRADED
         self.mock_state.service_health_state_str = "Service not responding"
 
         response = await self.view.health()
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
         data = json.loads(await response.get_data(as_text=True))
-        self.assertEqual(data["status"], health_enums.STATUS_CRITICAL)
+        self.assertEqual(data["status"], ServiceDegradationStatus.CRITICAL.value)
         self.assertEqual(len(data["issues"]), 2)
         self.assertEqual(data["issues"][0]["component"], "database")
         self.assertEqual(data["issues"][0]["details"], "Slow queries detected")
