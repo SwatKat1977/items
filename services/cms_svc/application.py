f8c75ea75116aeb91059c587b453a07fa7b622c7
SwatKat1977 <5666718+SwatKat1977@users.noreply.github.com>
2025-11-26 19:10:56 +0000

    update to cleaner initialisation

--- services/cms_svc/application.py
+++ services/cms_svc/application.py
@@ -14,46 +14,29 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 import asyncio
-import logging
 import os
-from base_application import BaseApplication
+from items_common import __version__, LICENSE_TEXT, SERVICE_COPYRIGHT_TEXT
+from items_common.base_microservice import BaseMicroservice
 from configuration_layout import CONFIGURATION_LAYOUT
-from logging_consts import LOGGING_DATETIME_FORMAT_STRING, \
-                           LOGGING_DEFAULT_LOG_LEVEL, \
-                           LOGGING_LOG_FORMAT_STRING
 from threadsafe_configuration import ThreadSafeConfiguration as Configuration
-from version import BUILD_TAG, BUILD_VERSION, RELEASE_VERSION, \
-                    SERVICE_COPYRIGHT_TEXT, LICENSE_TEXT
 from apis import create_api_routes
-from state_object import StateObject
 
 
-class Application(BaseApplication):
-    """ ITEMS Accounts Service """
+class Service(BaseMicroservice):
+    """ ITEMS CMS Service """
+
+    CONFIG_FILE_ENV: str = "ITEMS_CMS_SVC_CONFIG_FILE"
+    CONFIG_REQUIRED_ENV: str = "ITEMS_CMS_SVC_CONFIG_FILE_REQUIRED"
 
     def __init__(self, quart_instance):
         super().__init__()
         self._quart_instance = quart_instance
-        self._state_object: StateObject = StateObject()
+        self._service_state.database_enabled = True
 
-        self._logger = logging.getLogger(__name__)
-        log_format= logging.Formatter(LOGGING_LOG_FORMAT_STRING,
-                                      LOGGING_DATETIME_FORMAT_STRING)
-        console_stream = logging.StreamHandler()
-        console_stream.setFormatter(log_format)
-        self._logger.setLevel(LOGGING_DEFAULT_LOG_LEVEL)
-        self._logger.addHandler(console_stream)
-
-    def _initialise(self) -> bool:
-
-        build = f"V{RELEASE_VERSION}-{BUILD_VERSION}{BUILD_TAG}"
-
-        self._logger.info('ITEMS CMS Microservice %s', build)
+    async def _initialise(self) -> bool:
+        self._logger.info('ITEMS CMS Microservice %s', __version__)
         self._logger.info(SERVICE_COPYRIGHT_TEXT)
         self._logger.info(LICENSE_TEXT)
-
-        # Set the version string on state object.
-        self._state_object.version = build
 
         if not self._manage_configuration():
             return False
@@ -63,7 +46,7 @@ class Application(BaseApplication):
         self._logger.setLevel(Configuration().logging_log_level)
 
         self._quart_instance.register_blueprint(
-            create_api_routes(self._logger, self._state_object), url_prefix="")
+            create_api_routes(self._logger, self._service_state), url_prefix="")
 
         return True
 
@@ -71,35 +54,22 @@ class Application(BaseApplication):
         """ Abstract method for main application. """
         await asyncio.sleep(0.1)
 
-    def _shutdown(self):
+    async def _shutdown(self):
         """ Abstract method for application shutdown. """
 
     def _manage_configuration(self) -> bool:
         """
         Manage the service configuration.
         """
-
-        config_file = os.getenv("ITEMS_CMS_SVC_CONFIG_FILE", None)
-
-        config_file_required_str: str = os.getenv(
-            "ITEMS_CMS_SVC_CONFIG_FILE_REQUIRED", None)
-
-        config_file_required: bool = False
-        if config_file_required_str is not None and config_file_required_str == "1":
-            config_file_required = True
-
-        self._logger.info("Configuration file required? %s",
-                          "True" if config_file_required else "False")
-
-        if not config_file and config_file_required:
-            self._logger.critical("Configuration file missing!")
+        error_status, required, config_file = self._check_for_configuration(
+            self.CONFIG_FILE_ENV,self.CONFIG_REQUIRED_ENV)
+        if error_status:
+            self._logger.critical(error_status)
             return False
 
-        if config_file_required:
-            self._logger.info("Configuration file : %s", config_file)
-
-        Configuration().configure(CONFIGURATION_LAYOUT, config_file,
-                                  config_file_required)
+        Configuration().configure(CONFIGURATION_LAYOUT,
+                                  config_file,
+                                  required)
 
         try:
             Configuration().process_config()
@@ -107,6 +77,8 @@ class Application(BaseApplication):
         except ValueError as ex:
             self._logger.critical("Configuration error : %s", str(ex))
             return False
+
+        backend_db_filename: str = Configuration().backend_db_filename
 
         self._logger.info("Configuration")
         self._logger.info("=============")
@@ -116,7 +88,11 @@ class Application(BaseApplication):
                           Configuration().logging_log_level)
 
         self._logger.info("[Backend]")
-        self._logger.info("=> Database filename : %s",
-                          Configuration().backend_db_filename)
+        self._logger.info("=> Database filename : %s", backend_db_filename)
+
+        if not os.path.isfile(backend_db_filename):
+            self._logger.critical("Database file '%s' not found!",
+                                  backend_db_filename)
+            return False
 
         return True
