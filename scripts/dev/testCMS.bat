32664ef472bf8aa874b2a341c2d303d4f6262653
SwatKat1977 <5666718+SwatKat1977@users.noreply.github.com>
2025-11-28 00:14:58 +0000

    added windows scripts

---
+++ scripts/dev/testCMS.bat
@@ -0,0 +1,5 @@
+set PYTHONPATH=items/shared;unit_tests/cms_svc;services/cms_svc;common
+set ITEMS_CMS_SVC_CONFIG_FILE=
+
+coverage run --rcfile=.github/workflows/.coveragerc_cms_svc -m unittest -v unit_tests/cms_svc/main.py
+coverage report -m
