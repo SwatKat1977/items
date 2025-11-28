32009aa6a527603d58a36fcc7a921976d0dab168
SwatKat1977 <5666718+SwatKat1977@users.noreply.github.com>
2025-11-28 01:24:12 +0000

    fix script

--- scripts/dev/testWebPortal.bat
+++ scripts/dev/testWebPortal.bat
@@ -1,4 +1,4 @@
 rem @echo off
-SET PYTHONPATH=items/shared;services/web_portal_svc;unit_tests/web_portal_svc
+SET PYTHONPATH=items/shared;services/web_portal_svc;unit_tests/web_portal_svc;common
 coverage run --rcfile=.github/workflows/.coveragerc_web_portal_svc -m unittest -v unit_tests/web_portal_svc/main.py
 coverage report -m
