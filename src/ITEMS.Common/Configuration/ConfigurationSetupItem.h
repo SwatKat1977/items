b6ddec6a63b7f85cb236a0c828558d183bee894e
SwatKat1977 <5666718+SwatKat1977@users.noreply.github.com>
2026-02-24 20:02:04 +0000

    cleanup

--- src/ITEMS.Common/Configuration/ConfigurationSetupItem.h
+++ src/ITEMS.Common/Configuration/ConfigurationSetupItem.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2025 Integrated Test Management Suite Development Team
+Copyright 2026 Integrated Test Management Suite Development Team
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
