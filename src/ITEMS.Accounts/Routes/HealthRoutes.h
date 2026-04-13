9b75f9c7b6a303f1540506162d4f18aef9343a8e
SwatKat1977 <5666718+SwatKat1977@users.noreply.github.com>
2026-04-13 22:42:45 +0100

    added bare routes

--- src/ITEMS.Accounts/Routes/HealthRoutes.h
+++ src/ITEMS.Accounts/Routes/HealthRoutes.h
@@ -1,5 +1,29 @@
-#pragma once
-class HealthRoutes
-{
-};
+/*
+Copyright 2026 Integrated Test Management Suite Development Team
 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http ://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+#ifndef HEALTH_ROUTES_H_
+#define HEALTH_ROUTES_H_
+#include "CrowIncludes.h"
+
+namespace ITEMS::Accounts {
+
+    class HealthRoutes {
+    public:
+        static void Register(crow::SimpleApp& app);
+    };
+
+} // namespace ITEMS::Accounts
+
+#endif
