54c14c90e13312f467e0d58144a1f089bf208d1a
SwatKat1977 <5666718+SwatKat1977@users.noreply.github.com>
2026-05-21 01:32:05 +0100

    Remove PyCharm project files

--- scripts/dev/runAccounts.bat
+++ scripts/dev/runAccounts.bat
@@ -1,9 +1,10 @@
 @echo off
-set PYTHONPATH=services/accounts_svc;items/shared;common
-set QUART_APP=services/accounts_svc
-set QUART_DEBUG=1
-set ITEMS_ACCOUNTS_SVC_CONFIG_FILE_REQUIRED=0
+echo Starting ITEMS - Identity Service
+rem set PYTHONPATH=.
+set QUART_APP=items/services/items_identity
+rem set ITEMS_IDENTITY_CONFIG_FILE_REQUIRED=1
+rem set ITEMS_IDENTITY_CONFIG_FILE=configs/svc.cfg
+rem set BACKEND_DB_FILENAME=databases/items_accounts_svc.db
 set LOGGING_LOG_LEVEL=DEBUG
-set BACKEND_DB_FILENAME=databases/items_accounts_svc.db
-rem source .venv/bin/activate
-quart run -p 4000
+
+python -m items.services.items_identity.run
