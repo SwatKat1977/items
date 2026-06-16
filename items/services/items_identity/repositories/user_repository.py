f527153d4db50b713b4fd293df2043398cc87327
SwatKat1977 <5666718+SwatKat1977@users.noreply.github.com>
2026-06-16 23:58:45 +0100

    Cleaned up user repository

--- items/services/items_identity/repositories/user_repository.py
+++ items/services/items_identity/repositories/user_repository.py
@@ -10,41 +10,47 @@ class UserRepository:
     Repository responsible for user-related persistence operations.
     """
 
+    GET_USER_FOR_LOGON_QUERY: str = (
+            "SELECT id, logon_type, account_status "
+            "FROM user_profile "
+            "WHERE email_address = ?")
+
+    GET_PASSWORD_HASH_QUERY: str = (
+        "SELECT password "
+        "FROM user_auth_details "
+        "WHERE user_id = ?")
+
     def __init__(self,
                  logger: logging.Logger,
                  config: IdentityConfiguration) -> None:
         self._logger: logging.Logger = logger.getChild(__name__)
         self._config: IdentityConfiguration = config
 
-        self._db = SqliteInterface(logger,
-                                   self._config.backend_db_filename)
+        self._db: SqliteInterface = SqliteInterface(
+            self._logger,
+            self._config.backend_db_filename)
 
-    def get_user_for_logon(self, email: str) -> Optional[tuple]:
+    async def get_user_by_email(self,
+                                email: str) -> Optional[tuple[int, int, int]]:
         """
         Retrieve user logon information by email address.
 
         Returns:
-            tuple:
+            Tuple containing:
                 (
                     user_id,
                     logon_type,
                     account_status
                 )
 
-            or None if the user does not exist.
+            or None if no matching user exists.
         """
 
-        query = (
-            "SELECT id, logon_type, account_status "
-            "FROM user_profile "
-            "WHERE email_address = ?")
+        return await self._db.run_query(self.GET_USER_FOR_LOGON_QUERY,
+                                        (email,),
+                                        fetch_one=True)
 
-        return self._db.run_query(
-            query,
-            (email,),
-            fetch_one=True)
-
-    def get_password_hash(
+    async def get_password_hash(
             self,
             user_id: int) -> Optional[bytes]:
         """
@@ -54,17 +60,8 @@ class UserRepository:
             Password hash bytes or None if not found.
         """
 
-        query = (
-            "SELECT password "
-            "FROM user_auth_details "
-            "WHERE user_id = ?")
+        row = await self._db.run_query(self.GET_PASSWORD_HASH_QUERY,
+                                       (user_id,),
+                                       fetch_one=True)
 
-        row = self._db.run_query(
-            query,
-            (user_id,),
-            fetch_one=True)
-
-        if not row:
-            return None
-
-        return row[0]
+        return row[0] if row else None
