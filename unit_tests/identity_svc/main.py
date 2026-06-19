afaddb1f29046ef6d303d653a337c60006c5afcd
SwatKat1977 <paulmorriss1977@gmail.com>
2026-06-19 01:08:11 +0100

    update to tests

--- unit_tests/identity_svc/main.py
+++ unit_tests/identity_svc/main.py
@@ -1,6 +1,5 @@
 import unittest
 from test_application import TestApplication
-from test_threadsafe_configuration import TestIdentityConfiguration
 from test_da_user_data_access_layer import TestUserRepository
 from test_apis_authentication_api import TestAuthenticatePasswordHandler
 from test_apis_health_api import TestHealthHandler
