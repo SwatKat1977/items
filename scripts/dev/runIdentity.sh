529fbc3253020ab9fa6caf69e4a6570e1f1d34f0
SwatKat1977 <paulmorriss1977@gmail.com>
2026-06-19 22:33:58 +0100

    update

--- scripts/dev/runIdentity.sh
+++ scripts/dev/runIdentity.sh
@@ -3,6 +3,6 @@ export QUART_APP=items/services/items_identity
 export QUART_DEBUG=1
 #export ITEMS_ACCOUNTS_SVC_CONFIG_FILE_REQUIRED=0
 export LOGGING_LOG_LEVEL=DEBUG
-export BACKEND_DB_FILENAME=databases/items_accounts_svc.db
+export BACKEND_DB_FILENAME=databases/items_identity.LATEST.db
 
 python -m items.services.items_identity.run
