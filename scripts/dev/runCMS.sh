71695aa0c3c43306483a0555666c686e65278408
SwatKat1977 <paulmorriss1977@gmail.com>
2026-06-21 00:22:35 +0100

    update

--- scripts/dev/runCMS.sh
+++ scripts/dev/runCMS.sh
@@ -1,7 +1,7 @@
-export PYTHONPATH=items/cms_svc:items/shared
-export QUART_APP=items/cms_svc
-export ITEMS_CMS_SVC_CONFIG_FILE_REQUIRED=0
-export ITEMS_CMS_SVC_CONFIG_FILE=cms.cfg
+export PYTHONPATH=.
+export QUART_APP=items/services/items_cms
+export ITEMS_CMS_CONFIG_FILE_REQUIRED=0
+export ITEMS_CMS_CONFIG_FILE=cms.cfg
 export LOGGING_LOG_LEVEL=DEBUG
 
-quart run -p 5050 --reload
+python -m items.services.items_cms.run
