ff7e1b8cd3fd69e3b99401655e020ae51b3017a0
SwatKat1977 <paulmorriss1977@gmail.com>
2026-06-21 12:23:21 +0100

    Clean up of CMS

--- scripts/dev/runCMS.sh
+++ scripts/dev/runCMS.sh
@@ -1,7 +1,8 @@
 export PYTHONPATH=.
 export QUART_APP=items/services/items_cms
-export ITEMS_CMS_CONFIG_FILE_REQUIRED=0
-export ITEMS_CMS_CONFIG_FILE=cms.cfg
+export ITEMS_CMS_CONFIG_FILE_REQUIRED=1
+export ITEMS_CMS_CONFIG_FILE=configs/cms.cfg
 export LOGGING_LOG_LEVEL=DEBUG
+export BACKEND_DB_FILENAME=databases/items_cms.LATEST.db
 
 python -m items.services.items_cms.run
