06725897c16ccaec4b1388319f1b5a183bc757eb
SwatKat1977 <paulmorriss1977@gmail.com>
2026-06-21 02:04:35 +0100

    Cleanup of cms config layout

--- items/services/items_cms/configuration_layout.py
+++ items/services/items_cms/configuration_layout.py
@@ -1,5 +1,5 @@
 """
-Copyright 2025 Integrated Test Management Suite Development Team
+Copyright 2025-2026 Integrated Test Management Suite Development Team
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -13,8 +13,8 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-from configuration import configuration_setup
-
+from weaver_framework.configuration_system.configuration_setup import (
+    ConfigItemDataType, ConfigurationSetup, ConfigurationSetupItem)
 # pylint: disable=too-few-public-methods
 
 
@@ -24,28 +24,28 @@ class ConfigurationConstants:
     SECTION_LOGGING: str = 'logging'
     SECTION_BACKEND: str = 'backend'
 
-    ITEM_LOGGING_LOG_LEVEL: str = 'log_level'
+    LOGGING_LOG_LEVEL: str = 'log_level'
     LOG_LEVEL_DEBUG: str = 'DEBUG'
     LOG_LEVEL_INFO: str = 'INFO'
 
-    ITEM_BACKEND_DB_FILENAME: str = 'db_filename'
+    BACKEND_DB_FILENAME: str = 'db_filename'
 
 
-CONFIGURATION_LAYOUT = configuration_setup.ConfigurationSetup(
+CONFIGURATION_LAYOUT = ConfigurationSetup(
     {
         ConfigurationConstants.SECTION_LOGGING: [
-            configuration_setup.ConfigurationSetupItem(
-                ConfigurationConstants.ITEM_LOGGING_LOG_LEVEL,
-                configuration_setup.ConfigItemDataType.STRING,
+            ConfigurationSetupItem(
+                ConfigurationConstants.LOGGING_LOG_LEVEL,
+                ConfigItemDataType.STRING,
                 valid_values=[ConfigurationConstants.LOG_LEVEL_DEBUG,
                               ConfigurationConstants.LOG_LEVEL_INFO],
                 default_value=ConfigurationConstants.LOG_LEVEL_INFO)
         ],
         ConfigurationConstants.SECTION_BACKEND: [
-            configuration_setup.ConfigurationSetupItem(
-                ConfigurationConstants.ITEM_BACKEND_DB_FILENAME,
-                configuration_setup.ConfigItemDataType.STRING,
-                default_value="items_cms_svc.db")
+            ConfigurationSetupItem(
+                ConfigurationConstants.BACKEND_DB_FILENAME,
+                ConfigItemDataType.STRING,
+                default_value="items_cms.db")
         ]
     }
 )
