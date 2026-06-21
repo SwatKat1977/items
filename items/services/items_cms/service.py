e0fa58393014d7722a76e5905d93c92f4578a78d
SwatKat1977 <paulmorriss1977@gmail.com>
2026-06-21 10:42:52 +0100

    Clean up of CMS

--- items/services/items_cms/service.py
+++ items/services/items_cms/service.py
@@ -13,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+'''
 import asyncio
 import os
 from items_common import __version__, LICENSE_TEXT, SERVICE_COPYRIGHT_TEXT
@@ -96,3 +97,98 @@ class Service(BaseMicroservice):
             return False
 
         return True
+'''
+
+
+import asyncio
+from pathlib import Path
+from quart import Quart
+from weaver_framework.microservice.base_microservice import BaseMicroservice
+from items.shared import LICENSE_TEXT, SERVICE_COPYRIGHT_TEXT, __version__
+from items.shared.service_state import ServiceState
+from items.services.items_cms.configuration_layout import \
+    CONFIGURATION_LAYOUT
+from items.services.items_cms.cms_configuration import \
+    CMSConfiguration
+
+
+class Service(BaseMicroservice):
+    """ ITEMS CMS Microservice """
+
+    CONFIG_FILE_ENV: str = "ITEMS_CMS_CONFIG_FILE"
+    CONFIG_REQUIRED_ENV: str = "ITEMS_CMS_CONFIG_FILE_REQUIRED"
+
+    def __init__(self, quart_instance: Quart):
+        super().__init__()
+        self._quart_instance = quart_instance
+        self._service_state: ServiceState = ServiceState()
+        self._config: CMSConfiguration = CMSConfiguration()
+
+        self._service_state.database_enabled = True
+
+    async def _initialise(self) -> bool:
+        self.logger.info('ITEMS CMS Microservice %s', __version__)
+        self.logger.info(SERVICE_COPYRIGHT_TEXT)
+        self.logger.info(LICENSE_TEXT)
+
+        self._service_state.version = __version__
+
+        if not self._manage_configuration():
+            return False
+
+        self.logger.info('Setting logging level to %s',
+                         self._config.logging_log_level)
+        self.logger.setLevel(self._config.logging_log_level)
+
+        db_filename: Path = Path(self._config.backend_db_filename)
+        if not db_filename.is_file():
+            self._logger.critical("Backend database file '%s' is missing!",
+                                  db_filename)
+            return False
+
+        return True
+
+    async def _create_tasks(self) -> list[asyncio.Task]:
+        """ Create and return the service's background tasks. """
+        return [asyncio.create_task(self._dummy_task())]
+
+    async def _shutdown(self):
+        """ Abstract method for application shutdown. """
+
+    def _manage_configuration(self) -> bool:
+        """
+        Manage the service configuration.
+        """
+        error_status, required, config_file = self._check_for_configuration(
+            self.CONFIG_FILE_ENV,self.CONFIG_REQUIRED_ENV)
+        if error_status:
+            self._logger.critical(error_status)
+            return False
+
+        self._config.configure(CONFIGURATION_LAYOUT, config_file, required)
+
+        try:
+            self._config.process_config()
+
+        except ValueError as ex:
+            self._logger.critical("Configuration error : %s", str(ex))
+            return False
+
+        self._logger.info("Configuration")
+        self._logger.info("=============")
+
+        self._logger.info("Configuration file required: %s",
+                          "True" if required else "False")
+        self._logger.info("Configuration file : %s",
+                          "None"if not required else config_file)
+        self._logger.info("[logging]")
+        self._logger.info("=> Logging log level : %s",
+                          self._config.logging_log_level)
+        self._logger.info("[Backend]")
+        self._logger.info("=> Database filename : %s",
+                          self._config.backend_db_filename)
+
+        return True
+
+    async def _dummy_task(self) -> None:
+        await self.shutdown_event.wait()
