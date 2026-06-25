2f5738fe9b65212b2630e7b6f8131ec5db25b1c1
SwatKat1977 <5666718+SwatKat1977@users.noreply.github.com>
2026-06-25 21:53:42 +0100

    Fix for schemas

--- items/tool_cms_db_builder/sql_values.py
+++ items/tool_cms_db_builder/sql_values.py
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-import databases.cms_db_tables as cms_db_tables
+import cms_db_tables as cms_db_tables
 
 """
 Table: projects
