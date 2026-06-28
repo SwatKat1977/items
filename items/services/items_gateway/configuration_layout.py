f60b49d4354b6a6526ba91fa8544db34d017561d
SwatKat1977 <5666718+SwatKat1977@users.noreply.github.com>
2026-06-28 16:43:39 +0100

    rework

--- items/services/items_gateway/configuration_layout.py
+++ items/services/items_gateway/configuration_layout.py
@@ -1,5 +1,5 @@
 """
-Copyright 2025 Integrated Test Management Suite Development Team
+Copyright 2025-2026 Integrated Test Management Suite Development Team
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -13,8 +13,8 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-from configuration import configuration_setup
-
+from weaver_framework.configuration_system.configuration_setup import (
+    ConfigItemDataType, ConfigurationSetup, ConfigurationSetupItem)
 # pylint: disable=too-few-public-methods
 
 
@@ -41,43 +41,43 @@ class ConfigurationConstants:
     APIS_WEB_PORTAL_SVC_DEFAULT: str = "http://localhost:8080/"
 
 
-CONFIGURATION_LAYOUT = configuration_setup.ConfigurationSetup(
+CONFIGURATION_LAYOUT = ConfigurationSetup(
     {
         ConfigurationConstants.SECTION_LOGGING: [
-            configuration_setup.ConfigurationSetupItem(
+            ConfigurationSetupItem(
                 ConfigurationConstants.LOGGING_LOG_LEVEL,
-                configuration_setup.ConfigItemDataType.STRING,
+                ConfigItemDataType.STRING,
                 valid_values=[ConfigurationConstants.LOGGING_LOG_LEVEL_DEBUG,
                               ConfigurationConstants.LOGGING_LOG_LEVEL_INFO],
                 default_value=ConfigurationConstants.LOGGING_LOG_LEVEL_INFO)
         ],
 
         ConfigurationConstants.SECTION_GENERAL: [
-            configuration_setup.ConfigurationSetupItem(
+            ConfigurationSetupItem(
                 ConfigurationConstants.GENERAL_METADATA_CONFIG_FILE,
-                configuration_setup.ConfigItemDataType.STRING,
+                ConfigItemDataType.STRING,
                 default_value=
                 ConfigurationConstants.GENERAL_METADATA_CONFIG_FILE_DEFAULT),
-            configuration_setup.ConfigurationSetupItem(
+            ConfigurationSetupItem(
                 ConfigurationConstants.GENERAL_API_SIGNING_SECRET,
-                configuration_setup.ConfigItemDataType.STRING,
+                ConfigItemDataType.STRING,
                 is_required=True),
         ],
 
         ConfigurationConstants.SECTION_APIS: [
-            configuration_setup.ConfigurationSetupItem(
+            ConfigurationSetupItem(
                 ConfigurationConstants.APIS_ACCOUNTS_SVC,
-                configuration_setup.ConfigItemDataType.STRING,
+                ConfigItemDataType.STRING,
                 default_value=
                 ConfigurationConstants.APIS_ACCOUNTS_SVC_DEFAULT),
-            configuration_setup.ConfigurationSetupItem(
+            ConfigurationSetupItem(
                 ConfigurationConstants.APIS_CMS_SVC,
-                configuration_setup.ConfigItemDataType.STRING,
+                ConfigItemDataType.STRING,
                 default_value=
                 ConfigurationConstants.APIS_CMS_SVC_DEFAULT),
-            configuration_setup.ConfigurationSetupItem(
+            ConfigurationSetupItem(
                 ConfigurationConstants.APIS_WEB_PORTAL_SVC,
-                configuration_setup.ConfigItemDataType.STRING,
+                ConfigItemDataType.STRING,
                 default_value=
                 ConfigurationConstants.APIS_WEB_PORTAL_SVC_DEFAULT)
         ]
