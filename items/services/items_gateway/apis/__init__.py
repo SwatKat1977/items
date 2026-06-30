13b3e3f77fdbb9ea1d6403e417a61a675c4f3eeb
SwatKat1977 <5666718+SwatKat1977@users.noreply.github.com>
2026-06-30 20:56:37 +0100

    changes

---
+++ items/services/items_gateway/apis/__init__.py
@@ -0,0 +1,54 @@
+"""
+Copyright 2025-2026 Integrated Test Management Suite Development Team
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+import logging
+import quart
+from weaver_framework.microservice.rest_client import RestClient
+from items.services.items_gateway.sessions import Sessions
+from items.services.items_gateway.apis.web.session_api import create_sessions_routes
+from items.services.items_gateway.gateway_configuration import GatewayConfiguration
+
+
+def create_routes(logger: logging.Logger,
+                  sessions: Sessions,
+                  configuration: GatewayConfiguration,
+                  rest_client: RestClient) -> quart.Blueprint:
+    """Create and register all CMS API routes.
+
+    This function creates the root API blueprint for the Gateway service and
+    registers all feature-specific route blueprints.
+
+    Args:
+        logger: Logger instance used for route registration and diagnostics.
+        sessions: User sessions.
+        configuration (GatewayConfiguration): Gateway configuration.
+        rest_client (RestClient) Instance of rest_client.
+
+    Returns:
+        A Quart blueprint containing all registered Gateway API routes.
+    """
+    routes_bp = quart.Blueprint("api_routes", __name__)
+
+    routes_bp.register_blueprint(create_sessions_routes(logger,
+                                                        sessions,
+                                                        configuration,
+                                                        rest_client))
+    '''
+    routes_bp.register_blueprint(create_testcases_routes(logger,
+                                                         state,
+                                                         configuration))
+    '''
+
+    return routes_bp
