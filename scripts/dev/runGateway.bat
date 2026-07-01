13efe813841ba1a325deb91fdfff6426808ca136
SwatKat1977 <5666718+SwatKat1977@users.noreply.github.com>
2026-07-02 00:50:21 +0100

    updated

--- scripts/dev/runGateway.bat
+++ scripts/dev/runGateway.bat
@@ -6,8 +6,7 @@ SET ITEMS_GATEWAY_SVC_CONFIG_FILE=configs/gateway.cfg
 SET ITEMS_GATEWAY_SVC_CONFIG_FILE_REQUIRED=1
 SET GENERAL_API_SIGNING_SECRET=ApiSigningSecret
 SET GENERAL_METADATA_CONFIG_FILE=configs/metadata.config
-SET APIS_ACCOUNTS_SVC=http://localhost:4000/
-SET APIS_CMS_SVC=http://localhost:5050/
+@rem SET APIS_CMS_SVC=http://localhost:5050/
 set LOGGING_LOG_LEVEL=DEBUG
 
 python -m items.services.items_gateway.run
