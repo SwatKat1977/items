47cde789b3724fcd9fdf38715749493181d781e7
SwatKat1977 <5666718+SwatKat1977@users.noreply.github.com>
2026-07-07 00:48:46 +0100

    Fixed path

--- scripts/dev/testIdentity.bat
+++ scripts/dev/testIdentity.bat
@@ -1,4 +1,4 @@
-set PYTHONPATH=services/identity_svc;unit_tests/items_identity
+set PYTHONPATH=.;items/services/items_identity;unit_tests/items_identity
 set ITEMS_IDENTITY_CONFIG_FILE=
 
 coverage run --rcfile=.github/workflows/.coveragerc_identity_svc -m unittest -v unit_tests/items_identity/main.py
