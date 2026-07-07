7774a015e89515e4b8336547497e620f85c60d52
SwatKat1977 <5666718+SwatKat1977@users.noreply.github.com>
2026-07-07 23:26:48 +0100

    Change to pass in a dataclass of class injections instead of lots of instances

---
+++ items/services/items_gateway/route_injections.py
@@ -0,0 +1,29 @@
+"""
+Copyright 2025-2026 Integrated Test Management Suite Development Team
+Copyright 2017-2025 INTMAC Development Team [Defunct]
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+from dataclasses import dataclass
+import logging
+from weaver_framework.microservice.rest_client import RestClient
+from items.services.items_gateway.sessions import Sessions
+from items.services.items_gateway.gateway_configuration import GatewayConfiguration
+
+
+@dataclass(frozen=True)
+class RouteInjections:
+    logger: logging.Logger | None = None
+    sessions: Sessions | None = None
+    configuration: GatewayConfiguration | None = None
+    rest_client: RestClient | None = None
