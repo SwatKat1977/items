c9a81ccaf2f29e8de127df8ac557d11c42d8300c
SwatKat1977 <5666718+SwatKat1977@users.noreply.github.com>
2026-07-11 10:17:30 +0100

    Auth logout page handler

---
+++ items/services/items_web_portal/page_handlers/auth/auth_logout_page_handler.py
@@ -0,0 +1,34 @@
+"""
+Copyright 2025-2026 Integrated Test Management Suite Development Team
+Copyright 2017-2025 INTMAC Development Team [Defunct]
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+import logging
+from weaver_framework.microservice.rest_client import RestClient
+from items.services.items_web_portal.configuration import Configuration
+from items.services.items_web_portal.portal_page_handler import (
+    PortalPageHandler)
+
+
+class AuthLogoutPageHandler(PortalPageHandler):
+
+    def __init__(self,
+                 logger: logging.Logger,
+                 config: Configuration,
+                 rest_client: RestClient):
+        super().__init__(logger, config, rest_client)
+
+    async def logout_page(self):
+        """ PLACEHOLDER """
+        return None
