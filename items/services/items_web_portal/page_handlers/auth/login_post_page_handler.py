fa8f933f9eb1a0f6a63e3e533a696cfed282c643
SwatKat1977 <5666718+SwatKat1977@users.noreply.github.com>
2026-07-11 20:39:08 +0100

    Cleanup

---
+++ items/services/items_web_portal/page_handlers/auth/login_post_page_handler.py
@@ -0,0 +1,99 @@
+"""
+Copyright 2025-2026 Integrated Test Management Suite Development Team
+Copyright 2017-2025 INTMAC Development Team [Defunct]
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+from items.shared.base_items_exception import BaseItemsException
+
+"""
+
+from base_web_view import PortalPageHandler
+from base_view import ApiResponse
+from quart import make_response, request, Response
+from base_items_exception import BaseItemsException
+import page_locations as pages
+from threadsafe_configuration import ThreadSafeConfiguration
+from metadata_settings import MetadataSettings
+"""
+
+from http import HTTPStatus
+import logging
+from quart import make_response, request, Response
+from weaver_framework.microservice.rest_client import RestClient
+from items.services.items_web_portal.configuration import Configuration
+import items.services.items_web_portal.page_locations as pages
+from items.services.items_web_portal.portal_page_handler import (
+    PortalPageHandler)
+
+
+class LoginPostPageHandler(PortalPageHandler):
+
+    def __init__(self,
+                 logger: logging.Logger,
+                 config: Configuration,
+                 rest_client: RestClient):
+        super().__init__(logger, config, rest_client)
+
+    async def login_post(self):
+        try:
+            if self._has_auth_cookies() and self._validate_cookies():
+                redirect = self._generate_redirect('')
+                response = await make_response(redirect)
+                return response
+
+        except BaseItemsException as ex:
+            self._logger.error('Internal Error: %s', ex)
+            return await self._render_page(pages.TEMPLATE_INTERNAL_ERROR_PAGE)
+
+        form_data = dict(await request.form)
+
+        user_email = form_data.get('user_email')
+        password = form_data.get('password')
+
+        if not user_email or not password:
+            error_msg = "Invalid username/password"
+            return await self._render_page(pages.TEMPLATE_LOGIN_PAGE,
+                                           generate_error_msg=True,
+                                           error_msg=error_msg)
+
+        auth_body: dict = {
+                "type": "basic",
+                "email_address": user_email,
+                "password": password
+        }
+        base_url: str = self._config.apis_gateway_svc
+        url = f"{base_url}web/session"
+
+        response: ApiResponse = await self._call_api_post(url, auth_body)
+
+        if response.status_code != HTTPStatus.OK:
+            self._logger.critical("Gateway svc request invalid - Reason: %s",
+                                  response.exception_msg)
+            error_msg = "Internal Error"
+            return await self._render_page(pages.TEMPLATE_LOGIN_PAGE,
+                                           generate_error_msg=True,
+                                           error_msg=error_msg)
+
+        if response.body.get("status") == 1:
+            redirect = self._generate_redirect('')
+            login_response: Response = await make_response(redirect)
+            login_response.set_cookie(self.COOKIE_USER, user_email)
+            login_response.set_cookie(self.COOKIE_TOKEN,
+                                      response.body.get("token"))
+            return login_response
+
+        error_msg = "Invalid username/password"
+        return await self._render_page(pages.TEMPLATE_LOGIN_PAGE,
+                                       generate_error_msg = True,
+                                       error_msg=error_msg)
