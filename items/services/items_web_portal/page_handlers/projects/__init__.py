e5cfeea9d7d2e8308a422486df6139c011bec996
SwatKat1977 <5666718+SwatKat1977@users.noreply.github.com>
2026-07-12 13:34:46 +0100

    Rework

--- items/services/items_web_portal/page_handlers/projects/__init__.py
+++ items/services/items_web_portal/page_handlers/projects/__init__.py
@@ -14,47 +14,35 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+'''
 import logging
-from quart import Blueprint
+
 from apis.projects_api_view import ProjectsApiView
 from metadata_settings import MetadataSettings
+'''
+from quart import Blueprint
+from items.services.items_web_portal.page_handler_injections import (
+    PageHandlerInjections)
+from .get_project_overview_page_handler import GetProjectOverviewPageHandler
 
 
-def create_blueprint(logger: logging.Logger,
-                     metadata: MetadataSettings) -> Blueprint:
-    """
-    Creates and registers a Quart Blueprint for handling HTML pages related to
-    projects.
+def create_projects_page_handlers(injections: PageHandlerInjections) -> Blueprint:
+    routes = Blueprint('projects_routes', __name__)
 
-    This function initializes a `View` object with Metadata settings and an
-    instance of logger, and then defines  API endpoints for projects pages.
+    handler_project_overview: GetProjectOverviewPageHandler = \
+        GetProjectOverviewPageHandler(injections.logger,
+                                      injections.config,
+                                      injections.rest_client)
 
-    Args:
-        logger (logging.Logger): A logger instance for logging messages.
-        metadata (MetadataSettings): A metadata settings instance.
+    injections.logger.debug(" Projects Page Handlers:")
 
-    Returns:
-        Blueprint: A Quart `Blueprint` object containing the registered route.
-    """
-    view = ProjectsApiView(logger, metadata)
+    # Index page: '/'
+    injections.logger.debug("=> %s GET /<int:project_id>",
+                            "Project overview page".ljust(40))
 
-    blueprint = Blueprint('test_cases_api', __name__)
-
-    logger.debug("-------------- Registering Projects routes ----------------")
-
-    logger.debug("=> /<project_id>/project_overview [GET] : Project overview "
-                 "(web page)")
-
-    @blueprint.route('/<project_id>/project_overview',
-                     methods=['GET'])
+    @routes.route('/<project_id>',
+                  methods=['GET'])
     async def project_overview_request(project_id: int):
-        return await view.project_overview(project_id)
+        return await handler_project_overview.project_overview(project_id)
 
-    logger.debug("=> /<project_id>/test_cases [GET]       : Project test cases "
-                 "(web page)")
-
-    @blueprint.route('/<project_id>/test_cases', methods=['GET'])
-    async def test_definitions_page_request(project_id: int):
-        return await view.test_cases(project_id)
-
-    return blueprint
+    return routes
