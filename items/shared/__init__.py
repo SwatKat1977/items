e23a7f126d182b471589671f8f21fbdf96443761
SwatKat1977 <5666718+SwatKat1977@users.noreply.github.com>
2026-07-26 22:50:33 +0100

    Bumped version (#80)

--- items/shared/__init__.py
+++ items/shared/__init__.py
@@ -17,11 +17,11 @@ limitations under the License.
 
 # Semantic version components
 MAJOR = 0
-MINOR = 0
+MINOR = 1
 PATCH = 0
 
 # e.g. "alpha", "beta", "rc1", or None
-PRE_RELEASE = "MVP #1"
+PRE_RELEASE = "Alpha Build"
 
 # Version tuple for comparisons
 VERSION = (MAJOR, MINOR, PATCH, PRE_RELEASE)
