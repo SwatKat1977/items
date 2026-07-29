896b8f7e598de5738218b0c1b1576c839f1b5441
SwatKat1977 <5666718+SwatKat1977@users.noreply.github.com>
2026-07-29 23:27:24 +0100

    gut tests

--- unit_tests/shared/main.py
+++ unit_tests/shared/main.py
@@ -1,9 +1,6 @@
 import unittest
-from test_base_application import TestBaseApplication
 from test_base_view import TestBaseView
 from test_configuration_manager import TestConfigurationManager
-from test_thread_safe_singleton import TestThreadSafeSingleton
-from test_base_sqlite_interface import TestBaseSqliteInterface
 
 if __name__ == "__main__":
     unittest.main()
