6c066fadebdc0939be8b44940b46f1a3b75cb8f0
SwatKat1977 <5666718+SwatKat1977@users.noreply.github.com>
2026-08-05 00:46:44 +0100

    invite routes

---
+++ items/services/items_identity/routes/invites/__init__.py
@@ -0,0 +1,69 @@
+"""
+Copyright 2025-2026 Integrated Test Management Suite Development Team
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+import logging
+import quart
+from items.services.items_identity.identity_configuration import (
+    IdentityConfiguration)
+from .create_invite_handler import CreateInviteHandler
+from .resend_invite_handler import ResendInviteHandler
+from .uninvite_handler import UninviteHandler
+
+
+def create_invite_routes(logger: logging.Logger,
+                         config: IdentityConfiguration) -> quart.Blueprint:
+    """Create and configure invite management routes.
+
+    Registered routes:
+
+    * ``POST /invites``          - Create a new pending invite.
+    * ``POST /invites/resend``   - Refresh token and expiry on an existing invite.
+    * ``POST /invites/uninvite`` - Cancel (soft-expire) a pending invite.
+
+    Args:
+        logger: Logger used for route registration and request handling.
+        config: Identity service configuration.
+
+    Returns:
+        A configured Quart blueprint containing all invite routes.
+    """
+    invite_routes = quart.Blueprint("invite_routes", __name__)
+
+    handler_create = CreateInviteHandler(logger, config)
+    handler_resend = ResendInviteHandler(logger, config)
+    handler_uninvite = UninviteHandler(logger, config)
+
+    logger.debug("Registering Invite API routes:")
+
+    logger.debug("=> %s POST /invites",
+                 "Create invite".ljust(40))
+    logger.debug("=> %s POST /invites/resend",
+                 "Resend invite".ljust(40))
+    logger.debug("=> %s POST /invites/uninvite",
+                 "Uninvite".ljust(40))
+
+    @invite_routes.route('/invites', methods=['POST'])
+    async def create_invite_request():
+        return await handler_create.create_invite()
+
+    @invite_routes.route('/invites/resend', methods=['POST'])
+    async def resend_invite_request():
+        return await handler_resend.resend_invite()
+
+    @invite_routes.route('/invites/uninvite', methods=['POST'])
+    async def uninvite_request():
+        return await handler_uninvite.uninvite()
+
+    return invite_routes
