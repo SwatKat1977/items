c5160569b240f51012fe0b211186a72cdeb30007
SwatKat1977 <5666718+SwatKat1977@users.noreply.github.com>
2026-08-11 13:19:04 +0100

    Bumped alpha build version

--- items/shared/__init__.py
+++ items/shared/__init__.py
@@ -21,7 +21,7 @@ MINOR = 1
 PATCH = 0
 
 # e.g. "alpha", "beta", "rc1", or None
-PRE_RELEASE = "Alpha Build 8"
+PRE_RELEASE = "Alpha Build 9"
 
 # Version tuple for comparisons
 VERSION = (MAJOR, MINOR, PATCH, PRE_RELEASE)
