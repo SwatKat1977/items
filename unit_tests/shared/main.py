abe624131a3ead2da649685a1b1007c46682e69e
SwatKat1977 <5666718+SwatKat1977@users.noreply.github.com>
2026-08-23 22:57:47 +0100

    bump version to 0.3.0 (#117)
    * bump version to 0.3.0
    
    * fix unit tests

--- unit_tests/shared/main.py
+++ unit_tests/shared/main.py
@@ -1,6 +1,7 @@
 import unittest
 from test_api_signature import TestApiSignature
 from test_service_state import TestServicerState
+from test_version import TestBuildVersionString
 
 if __name__ == "__main__":
     unittest.main()
